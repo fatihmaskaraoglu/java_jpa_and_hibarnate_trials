9894093961594e8243e5da83298d373c508bfe6b
fatihmaskaraoglu <fatihmaskaraoglu@gmail.com>
2023-06-11 14:59:45 +0300

    Setting up New Spring Boot Project for JPA and Hibernate

---
+++ src/test/java/com/myapp/springboot/learnjpaandhibarnate/LearnJpaAndHibarnateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springboot.learnjpaandhibarnate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHibarnateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
